343d66140a5e30a3fcc3e117427bb50899186dbd
mohammadvaladbiegi2 <mohammadvaladbiegi2@gmail.com>
2024-07-26 21:31:40 +0330

    Types State Hooks

--- src/component/Hooks/State.tsx
+++ src/component/Hooks/State.tsx
@@ -3,12 +3,18 @@ import React, { useState } from 'react';
 // type textType = string 
 // type UsersType = {}[] |  null
 
+// interface usersType {
+//     name:string,
+//     password : string | number
+// }
+
 const State = () => {
 
 // const [text, settext] = useState<textType>("");
 // const [Users, setUsers] = useState<UsersType>();
 // const [Conter, setConter] = useState<number>();
 // const [Product, setProduct] = useState<{id:number,title:string,isActive:boolean}[] | null>();
+// const [Users, setUsers] = useState<usersType>({} as usersType);
 
     return (
         <div>
